a7d71be014ebcfba421c5200deb516cd2e25cae7
liunanji <lnjasdf@qq.com>
2017-03-13 18:47:11 +0800

    ADD: 增加翻页爬取，随机停顿

--- youshu/spiders/NameSpider.py
+++ youshu/spiders/NameSpider.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from youshu.items import YoushuItem
+import time,random
 
 
 class NameSpider(scrapy.Spider):
@@ -10,7 +11,7 @@ class NameSpider(scrapy.Spider):
     #     'http://www.yousuu.com/category/all?page=2',
     # )
 
-    start_url = 'http://www.yousuu.com/category/all?page=2'
+    start_url = 'http://www.yousuu.com/category/all?page='
 
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
@@ -18,13 +19,16 @@ class NameSpider(scrapy.Spider):
     }
 
     def start_requests(self):
-        yield scrapy.Request(self.start_url, headers=self.headers)
+        yield scrapy.Request(self.start_url + str(1), headers=self.headers)
 
     def parse(self, response):
         books = response.xpath('//div[@class="booklist-item"]')
         for book in books:
             item = NameSpider.build_item(book)
             yield item
+        url = NameSpider.next_url(response)
+        if url is not None:
+            yield scrapy.Request(url, headers=self.headers)
 
     @staticmethod
     def build_item(book):
@@ -36,3 +40,17 @@ class NameSpider(scrapy.Spider):
         item['words'] = info[1]
         item['last_time'] = info[2]
         return item
+
+    @staticmethod
+    def next_url(response):
+        next_script = response.xpath('//ul[@class="pagination pull-right"]/li[last()]/a/@onclick').extract()[0]
+        last_page = int(next_script[next_script.find(',') + 1:next_script.find(')')])
+        current_page = int(response.url[response.url.find('=') + 1:])
+        if current_page == 5:  # TODO 测试5页
+            return None
+        if current_page < last_page:
+            second = random.randint(2, 10)  # 爬一页随机休息
+            print "sleep: %d" % (second,)
+            time.sleep(second)
+            return NameSpider.start_url + str(current_page + 1)
+        return None
