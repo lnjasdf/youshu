75f14d1d96c5067aa64159e483ceca88661886b1
liunanji <lnjasdf@qq.com>
2017-03-13 15:25:30 +0800

    ADD: 编辑item

--- youshu/items.py
+++ youshu/items.py
@@ -11,4 +11,5 @@ import scrapy
 class YoushuItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    name = scrapy.Field()
+    author = scrapy.Field()
